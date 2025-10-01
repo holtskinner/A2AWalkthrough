01b2402395e4eda3c91cd5d8bb99f85f3cebd9dc
Holt Skinner <13262395+holtskinner@users.noreply.github.com>
2025-10-01 15:33:22 -0500

    Update client to use rich

--- 3. A2AClient.py
+++ 3. A2AClient.py
@@ -1,6 +1,4 @@
 import asyncio
-import logging
-from typing import Any
 from uuid import uuid4
 
 import httpx
@@ -10,16 +8,20 @@ from a2a.types import (
     MessageSendParams,
     Part,
     Role,
-    SendMessageRequest,
     TextPart,
 )
+from rich.console import Console
+from rich.markdown import Markdown
 
 
 async def main() -> None:
     base_url = "http://0.0.0.0:9999"
 
     prompt = "How much would I pay for mental health therapy?"
-    async with httpx.AsyncClient() as httpx_client:
+
+    console = Console()
+
+    async with httpx.AsyncClient(timeout=10.0) as httpx_client:
         client = JsonRpcTransport(
             httpx_client=httpx_client,
             url=base_url,
@@ -33,8 +35,13 @@ async def main() -> None:
             )
         )
 
-        response = await client.send_message(request)
-        print(response.model_dump(mode="json", exclude_none=True))
+        response: Message = await client.send_message(request)
+
+        message_id = response.message_id
+        text_content = response.parts[0].root.text
+
+        console.print(f"[yellow]Message ID:[/] {message_id}")
+        console.print(Markdown(text_content))
 
 
 if __name__ == "__main__":
